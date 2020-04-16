6317122b4df293101d4b96131144d6155360d518
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-16 15:15:31 -0600

    react-router, react-router-dom, styled-components dependencies, and Homepage.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Homepage from './components/Homepage';
+
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MePic from '../images/ciana-tom.jpg';
+
+const Homepage = () => {
+
+    return (
+        <div>
+            <img src={MePic} alt='Thomas Barrett' />
+        </div>
+    )
+}
+
+export default Homepage
