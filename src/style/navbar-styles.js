46177882cf75838e315a923f234b89e326604806
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-18 11:31:13 -0600

    styled navbar

--- src/style/navbar-styles.js
+++ src/style/navbar-styles.js
@@ -2,7 +2,17 @@ import styled from 'styled-components';
 
 export const NavContainer = styled.nav `
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     width: 60%;
-    border: 1px solid black;
+    margin-bottom: 5%;
+    border: 1px solid white;
+    background-color: #454545;
+    opacity: 0.8;
+    
+    a {
+        text-decoration: none;
+        color: white;
+        padding: 3% 0;
+        font-size: 1.8rem;
+    }
 `
