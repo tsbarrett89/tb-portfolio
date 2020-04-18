6636715ca5c0712ed5e40bdaf87d0fb8050968c3
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-17 19:21:00 -0600

    app-style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router';
 
+import { AppContainer } from './style/app-style';
+
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-      <Navbar />
-
-      <Route exact path='/' />
+    <AppContainer>
+      <Route exact path='/' component={Homepage} />
       <Route path='/about' />
       <Route path='/skills' />
       <Route path='/projects' />
       <Route path='/contact' />
-    </div>
+      
+      <Navbar />
+    </AppContainer>
   );
 }
 
