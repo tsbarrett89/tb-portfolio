4eae9c1d01479c53cc0178b3716b8c3e11cd5121
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-22 15:43:54 -0600

    Save the Animals project added to projectsPage

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -7,6 +7,10 @@ const ProjectsPage = () => {
     return (
         <ProjectsContainer>
             <h2>Projects</h2>
+            <ProjectContainer>
+                <ProjectName>Save the Animals</ProjectName>
+                <ProjectDescription>This was my last build week project. I was the back-end engineer and Project Lead for a team of 4 students. I liked the project enough that I built out my own front end with React and expanded on the features. The site linked is entirely my work. Tech stack - Node.js with Express, React, Reactstrap, Styled-Components.</ProjectDescription>
+            </ProjectContainer>
         </ProjectsContainer>
     )
 }
