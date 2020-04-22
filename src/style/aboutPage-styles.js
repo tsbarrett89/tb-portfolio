4b236e6ef04a899c49c3c6f3a763a532524a618f
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-22 13:15:41 -0600

    styled AboutPage

--- src/style/aboutPage-styles.js
+++ src/style/aboutPage-styles.js
@@ -10,4 +10,16 @@ export const AboutContainer = styled.div `
     color: white;
     border: 3px solid white;
     border-radius: 10px;
+
+    h2 {
+        font-size: 2.2rem;
+        margin: 3% auto;
+    }
+    
+    p {
+        font-size: 1.2rem;
+        text-indent: 40px;
+        margin-bottom: 2%;
+        padding: 0 3%;
+    }
 `
