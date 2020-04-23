5ef4a1feb901ead4c027f2a61847dffe7a7d55e5
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-22 20:30:19 -0600

    Columns created

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-import { SkillsContainer } from '../style/skillsPage-styles'
+import { SkillsContainer, ColumnsContainer, Column } from '../style/skillsPage-styles'
 
 const SkillsPage = () => {
 
     return (
         <SkillsContainer>
             <h2>Skills and Experience</h2>
+            <ColumnsContainer>
+                <Column>
+                    <h4>Languages</h4>
+                </Column>
+                <Column>
+                    <h4>Libraries</h4>
+                </Column>
+                <Column>
+                    <h4>Frameworks</h4>
+                </Column>
+                <Column>
+                    <h4>Team Platforms</h4>
+                </Column>
+            </ColumnsContainer>
         </SkillsContainer>
     )
 }
