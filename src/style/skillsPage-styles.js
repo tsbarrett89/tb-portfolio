9cffc1adbdba72beb6728404b8163b26984f96dc
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-22 20:13:02 -0600

    skillsPage-styles created, SkillContainer

---
+++ src/style/skillsPage-styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const SkillsContainer = styled.div `
+    width: 60%;
+    background-color: #454545;
+    opacity: 0.8;
+    border: 3px solid white;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: white;
+`
