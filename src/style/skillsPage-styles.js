e63697f1ac6c173910c59fb568086413b6694e88
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-23 14:16:52 -0600

    styled skills

--- src/style/skillsPage-styles.js
+++ src/style/skillsPage-styles.js
@@ -13,6 +13,7 @@ export const SkillsContainer = styled.div `
 
     h2 {
         font-size: 2.5rem;
+        margin: 3% auto;
     }
 `
 
@@ -20,10 +21,21 @@ export const ColumnsContainer = styled.div `
     width: 100%;
     display: flex;
     justify-content: space-around;
+    margin-bottom: 3%;
 `
 
 export const Column = styled.div `
     width: 22%;
     display: flex;
     flex-direction: column;
+    margin-left: 3%;
+
+    ul {
+        font-size: 1.4rem;
+        margin-bottom: 2%;
+    }
+
+    li {
+        margin-bottom: 1.5%;
+    }
 `
